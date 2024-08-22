90d2b29c078dac162ce5d08647ff25f5f6a63987
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-22 15:29:10 -0300

    chore: Add postcss configuration, update npm dependencies, and remove unused router component

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,74 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Home from "../Pages/home";
-import Sobre from "../Pages/sobre";
-import Contato from "../Pages/contato";
-import Login from "../Pages/login";
-import Signup from "../Pages/signup";
-import logo from "../Images/logo.png";
+import logo from "../Images/logo.svg";
+import { MenuLink } from "./menuLink";
 
-export default function Header() {
+export default function Header({ buttonClicked, setButtonClicked, isLoggedIn }) {
   return (
-    <header>
-      <nav
-        className="navbar navbar-expand-md bg-info py-0 mx-0"
-        id="navbar"
-        style={{ width: "100%" }}
-      >
-	  <div className="container-md my-0 py-0">
-          <Link className="navbar-brand my-0" to="/">
-            <img
-              src={logo}
-              alt="Logo da empresa"
-              id="logo_nav"
-              className="img-fluid"
+    <nav className="bg-white border-b border-gray-200 sticky top-0 z-50 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            SÓ VIAGENS
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
             />
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" id="toggle-button"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse bg-info py-0 rounded-1 border border-info"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav ms-auto mb-lg-0">
-              <li className="nav-item navbar-links">
-                <Link className="nav-link ms-2 mt-2" to={Home}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item navbar-links">
-                <Link className="nav-link ms-2 mt-2" to={Sobre}>
-                  Sobre
-                </Link>
-              </li>
-              <li className="nav-item navbar-links">
-                <Link className="nav-link ms-2 mt-2" to={Contato}>
-                  Contato
-                </Link>
-              </li>
-              <li className="nav-item navbar-links">
-                <Link className="nav-link ms-2 mt-2" to={Login}>
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item navbar-links">
-                <Link className="nav-link ms-2 mt-2" to={Signup}>
-                  Signup
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+          </svg>
+        </button>
+        <MenuLink 
+          buttonClicked={buttonClicked} 
+          setButtonClicked={setButtonClicked} 
+          isLoggedIn={isLoggedIn} 
+        />
+      </div>
+    </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Rotas from './Routers/routers';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Pages/home';
+import { Sobre } from './Pages/sobre';
+import Servicos from './Pages/servicos';
+import Login from './Pages/login';
+import Signup from './Pages/signup';
+import { Container } from './Pages/container';
+import Dashboard from './Pages/dashboard';
+import ResetPassword from './Pages/reset-password';
+import Faq from './Pages/faq';
+import { checkAndLoadData } from './Configurations/loadDataToFirebase';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Container />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/sobre', element: <Sobre /> },
+        { path: '/servicos', element: <Servicos /> },
+        { path: '/login', element: <Login /> },
+        { path: '/signup', element: <Signup /> },
+        { path: '/dashboard', element: <Dashboard /> },
+        { path: '/reset-password', element: <ResetPassword /> },
+        { path: '/faq', element: <Faq /> },
+      ],
+    },
+  ]
+);
+
+checkAndLoadData();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Rotas />
-  </React.StrictMode>
+  <RouterProvider router={router} />,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
